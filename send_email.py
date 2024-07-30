c2b829d32e220fd6377c6151572797b75372afba
Satyam Bhat <satyambhat@aarkglobalinc.com>
2024-07-30 12:56:13 +0530

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.message import EmailMessage
+import os
+
+def send_email(sender, password, recipient, subject, body, attachment_path):
+    msg = EmailMessage()
+    msg['From'] = sender
+    msg['To'] = recipient
+    msg['Subject'] = subject
+    msg.set_content(body)
+
+    if os.path.exists(attachment_path):
+        with open(attachment_path, 'rb') as f:
+            file_data = f.read()
+            file_name = os.path.basename(attachment_path)
+        msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
+
+    with smtplib.SMTP('smtp-mail.outlook.com', 587) as server:
+        server.starttls()
+        server.login(sender, password)
+        server.send_message(msg)
+
+if __name__ == "__main__":
+    import sys
+    send_email(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
